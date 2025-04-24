9804b0811fdc3bf0d9ae7d8c138137760280e1f5
sowmaki <solene.tassel@hotmail.com>
2025-04-24 14:35:54 +0200

    :bug: fix: relative links on the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,13 @@ import { Home } from './pages/Home/Home';
 export default function App() {
 
   return (
-    <BrowserRouterRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         {/* <Route path="/portfolio" element={<Portfolio />} />
         <Route path="/project/:id" element={<Project />} /> */}
       </Routes>
-    </BrowserRouterRouter>
+    </BrowserRouter>
   )
 }
 
